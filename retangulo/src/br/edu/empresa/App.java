bdc3b432c70ed5341480744f347a255adb2f8012
RAFAELA DE ANDRADE MORAES <20272629@cfp127aluno.local>
2021-01-26 11:32:10 -0300

    Aula 26-01-21

---
+++ retangulo/src/br/edu/empresa/App.java
@@ -0,0 +1,16 @@
+package br.edu.empresa;
+
+import br.edu.empresa.gui.FrameRetangulo;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		FrameRetangulo tela = new FrameRetangulo();
+		tela.criarTela();
+		
+		
+		
+	}
+
+}
---
+++ retangulo/src/br/edu/empresa/model/Retangulo.java
@@ -0,0 +1,33 @@
+package br.edu.empresa.model;
+
+public class Retangulo {
+
+//	Atributos da conta (private)
+	private double base;
+	private double altura;
+	
+	public void setBase(double base) {
+		this.base = base;
+	}
+	
+	public double getBase() {
+		return this.base;
+	}
+	
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	public double getAltura() {
+		return altura;
+	}
+	
+//	Calcular a Área e Perímetro da conta (public)
+	public double calcularArea() {
+		return this.base * this.altura;
+	}
+	
+	public double calcularPerimetro() {
+		return 2 * (this.base + this.altura);
+	}
+}
